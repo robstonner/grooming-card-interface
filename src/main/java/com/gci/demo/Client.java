d0e5d46a372e6c6c1ae6f3e015d2009a0fbef8eb
robstonner <robstonner@gmail.com>
2021-04-07 22:20:42 -0500

    Added POST endpoint demo for object.

--- src/main/java/com/gci/demo/Client.java
+++ src/main/java/com/gci/demo/Client.java
@@ -1,11 +1,21 @@
 package com.gci.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Client {
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("phoneNumber")
     private String phoneNumber;
 
     public Client() {
         // no args constructor
+    }
+
+    public Client(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getName() {
--- src/main/java/com/gci/demo/DemoResource.java
+++ src/main/java/com/gci/demo/DemoResource.java
@@ -1,6 +1,8 @@
 package com.gci.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +12,22 @@ public class DemoResource {
 
     @GetMapping
     @RequestMapping("/")
-    public String demo() {
+    public String demoGet() {
         return "Hello World";
     }
 
     @GetMapping
-    @RequestMapping("/object")
+    @RequestMapping("/object/")
     public Client demoObject() {
         Client client = new Client();
         client.setName("Robert");
         client.setPhoneNumber("6604736583");
         return client;
+    }
+
+    @PostMapping
+    @RequestMapping("/post/")
+    public String demoPost(@RequestBody Client client) {
+        return "Hello " + client.getName() + "! I can call you at " + client.getPhoneNumber() + ".";
     }
 }
